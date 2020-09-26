525a43267c4be5ef9676a2c6f65b72a7d72c234d
pwner-web <68361304+pwner-web@users.noreply.github.com>
2020-09-27 01:28:37 +0800

    Add files via upload

---
+++ 4.流量分析/analysis.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+
+if __name__ == "__main__":
+    pcap = rdpcap('./aim.pcap')
+    for item in pcap:
+        if ('TCP' in item) and ('IP' in item):
+            src = item['IP'].fields['src']
+            dst = item['IP'].fields['dst']
+            sport = item['TCP'].fields['sport']
+            dport = item['TCP'].fields['dport']
+            if (src == "192.0.123" and sport == 80) or (dst == "192.168.0.123" and dport == 80):
+                print(repr(item))
+
+flag-{YKLTGULRGB}
